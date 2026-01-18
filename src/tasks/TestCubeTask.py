cb24f881b7ba0e08dba346236180beb65f4243fe
RogerJ <827090192@qq.com>
2026-01-19 00:47:38 +0800

    feature: 新增测试测试结果携带上dll结果

--- src/tasks/TestCubeTask.py
+++ src/tasks/TestCubeTask.py
@@ -1,3 +1,5 @@
+import ctypes
+import os
 
 from qfluentwidgets import FluentIcon
 
@@ -20,11 +22,20 @@ class TestCubeTask(MyBaseTask):
         from src.ocr_corrections_sub import dynamic_fix
         self.add_text_fix(dynamic_fix)
 
+        root_path = self.executor.config["project_root"]
+        # 加载dll
+        dll_path = os.path.join(root_path, 'libcube64.dll')
+        lib = ctypes.CDLL(dll_path)
+
+        self.check_func = lib['?washcube@@YAPEADPEBD00@Z']
+        self.check_func.restype = ctypes.c_char_p
+        self.check_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+
     def run(self) -> None:
         while True:
-            box1 = self.ocr(0.218, 0.44, 0.35, 0.475)
-            box2 = self.ocr(0.218, 0.474, 0.35, 0.508)
-            box3 = self.ocr(0.217, 0.503, 0.35, 0.537)
+            box1 = self.ocr(0.217, 0.44, 0.35, 0.48)
+            box2 = self.ocr(0.217, 0.472, 0.35, 0.51)
+            box3 = self.ocr(0.217, 0.5, 0.35, 0.54)
 
             ocr_res1 = "".join([b.name for b in box1]) if box1 else "没有识别到结果"
             ocr_res2 = "".join([b.name for b in box2]) if box2 else "没有识别到结果"
@@ -37,6 +48,12 @@ class TestCubeTask(MyBaseTask):
             self.info_set(key="第一条属性测试结果", value=True if res1 else False)
             self.info_set(key="第二条属性测试结果", value=True if res2 else False)
             self.info_set(key="第三条属性测试结果", value=True if res3 else False)
+
+            res_ptr: str = ctypes.cast(self.check_func(ocr_res1.encode('gbk'),
+                                                       ocr_res2.encode('gbk'),
+                                                       ocr_res3.encode('gbk')),
+                                       ctypes.c_char_p).value.decode('gbk')
+            self.info_set("dll校验结果为", res_ptr)
 
             if not res1 or not res2 or not res3:
                 self.info_set(key="第一条识别结果", value=ocr_res1)
