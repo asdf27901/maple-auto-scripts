04e180a6e490102a510c3da2fc2279fa876e2583
ok-oldking <ok-oldking@users.noreply.github.com>
2026-01-20 14:50:05 +0000

    fix: 修改众多bug

--- src/tasks/WashCubeTask.py
+++ src/tasks/WashCubeTask.py
@@ -18,7 +18,8 @@ class WashCubeTask(MyBaseTask):
             '魔方类型': [],
             '期望属性': [],
             '结果类型': [],
-            '需要洗的装备件数': 1
+            '需要洗的装备件数': 1,
+            '速率因子': 1.0,
         })
         self.config_type["魔方类型"] = {'type': "multi_selection",
                                         'options': ['Precious cube', 'Absolute cube']}
@@ -26,14 +27,16 @@ class WashCubeTask(MyBaseTask):
                                         'options': [
                                             '物攻', '魔攻',
                                             '力量', '敏捷', '智力', '运气', '血量', '全属性',
-                                            '1暴伤(只保留同属性大大大)', '2暴伤(不考虑第三条属性)', '3暴伤(怎么可能?)',
-                                            '1冷却(只保留同属性大大大)', '2冷却(不考虑第三条属性)', '3冷却(怎么可能?)'
+                                            '1爆伤(只保留同属性大大大)', '2爆伤(不考虑第三条属性)', '3爆伤(怎么可能?)',
+                                            '1冷却(只保留同属性大大大)', '2冷却(不考虑第三条属性)', '3冷却(怎么可能?)',
+                                            '测试(勾选了之后洗一下就换装备)'
                                         ]}
         self.config_type['结果类型'] = {'type': 'multi_selection',
                                         'options': ["大大大", "大大小"]}
         self.config_description.update({
             "魔方类型": "魔方会在已选中进行选择，\n优先珍贵附加魔方，如果都没有了则脚本结束",
-            "需要洗的装备件数": "会根据输入数量生成n*4的矩阵\n例如输入：11，生成如下：\n□ □ □ □\n□ ■ □ □\n□ □ ■\n只会找其中有颜色的格子洗"
+            "需要洗的装备件数": "会根据输入数量生成n*4的矩阵\n例如输入：11，生成如下：\n□ □ □ □\n□ ■ □ □\n□ □ ■\n只会找其中有颜色的格子洗",
+            "速率因子": "用于控制洗魔方速度，越小速率越快，建议值：1"
         })
 
         from src.ocr_corrections_sub import dynamic_fix
@@ -41,15 +44,19 @@ class WashCubeTask(MyBaseTask):
 
         root_path = self.executor.config["project_root"]
         # 加载dll
-        dll_path = os.path.join(root_path, 'libcube64.dll')
+        dll_path = os.path.join(root_path, 'src', 'libcube64.dll')
         lib = ctypes.CDLL(dll_path)
 
         self.check_func = lib['?washcube@@YAPEADPEBD00@Z']
         self.check_func.restype = ctypes.c_char_p
         self.check_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+        self.rate = 1
 
     def run(self) -> None:
         self.log_info(f"任务执行配置为：{self.config}")
+
+        self.rate = self.config['速率因子']
+        self.log_info(f"配置速率因子为: {self.config['速率因子']}")
         # 校验任务配置
         if not self.config['魔方类型'] \
                 or not self.config['期望属性'] \
@@ -62,16 +69,20 @@ class WashCubeTask(MyBaseTask):
             return
 
         # 判断是否打开了背包
-        box = self.open_bag()
-        if box is None:
+        if self.open_bag() is False:
             return
-        self.log_info(f"装备强化按钮坐标为：{box.center()}")
+
+        boxx = self.find_feature('button', horizontal_variance=1, vertical_variance=1, threshold=0.95)
+        if not boxx:
+            self.log_error('没有找到装备强化按钮，请重试')
+            return
+        self.log_info(f"装备强化按钮坐标为：{boxx[0].center()}")
         # 点击按钮中心坐标
         # 这里点3次是为了防止抓到了装备导致点击失败
-        self.click(*box.center())
-        self.click(*box.center())
-        self.click(*box.center())
-        self.sleep(1)
+        self.click(*boxx[0].center())
+        self.click(*boxx[0].center())
+        self.click(*boxx[0].center())
+        self.sleep(1 * self.rate)
 
         apd_box = self.find_feature("Additional potential deactivate", vertical_variance=0.1, horizontal_variance=0.1)
         if not apd_box:
@@ -81,7 +92,7 @@ class WashCubeTask(MyBaseTask):
         self.click(*apd_box[0].center())
         self.click(*apd_box[0].center())
 
-        self.sleep(1)
+        self.sleep(1 * self.rate)
         equ_boxes = (Box(
             x=625 + (i % 4) * 47,
             y=173 + (i >> 2) * 47,
@@ -90,17 +101,17 @@ class WashCubeTask(MyBaseTask):
         ) for i in range(self.config['需要洗的装备件数'] or 1))
 
         try:
-            self.wash_equipments(equ_boxes)
+            self.wash_equipments(equ_boxes, apd_box[0])
         except StopIteration:
             self.log_info("没有装备可以洗了")
 
-    def open_bag(self) -> Optional[Box]:
+    def open_bag(self) -> bool:
         """
         打开背包
         Returns:
         """
         self.click(0.5, 0.5)
-        self.sleep(1)
+        self.sleep(1 * self.rate)
 
         times = 0
         # 检查是否出现背包特征，如果没有出现，则再按下一次"i"
@@ -108,36 +119,40 @@ class WashCubeTask(MyBaseTask):
         while not box:
             times += 1
             self.send_key('i')
-            self.sleep(1)
+            self.sleep(1 * self.rate)
             if box := self.check_bag_is_opened():
                 break
             if times >= 3:
                 self.log_error('最终无法打开背包，退出程序')
-                return None
+                return False
+
+        self.mouse_down(*box.center())
+        self.sleep(0.5)
+        self.move(345, 126)
+        self.sleep(0.5)
+        self.mouse_up()
         self.log_info('背包打开成功')
-        return box
+        return True
 
     def check_bag_is_opened(self) -> Optional[Box]:
         """
         检查背包特征，判断是否已打开
         Returns:
         """
-        box1 = self.find_feature('button', horizontal_variance=1, vertical_variance=1, threshold=0.95)
-        box2 = self.find_feature('title', horizontal_variance=1, vertical_variance=1, threshold=0.95)
 
-        if not box1 and not box2:
+        box = self.find_feature('title', horizontal_variance=1, vertical_variance=1, threshold=0.95)
+
+        if not box:
             self.log_error('完全没有识别到背包特征，给钱帮忙检查, 1分钟50U')
             return None
-        if not box1 or not box2:
-            self.log_error('背包没有完全展示在游戏窗口画面中')
-            return None
-        return box1[0]
+        return box[0]
 
-    def wash_equipments(self, boxes: Generator):
+    def wash_equipments(self, boxes: Generator, apd_box: Box):
         """
         洗装备逻辑
         Args:
             boxes:
+            apd_box:
         Returns:
         """
         # 获取期望使用的魔方
@@ -160,8 +175,21 @@ class WashCubeTask(MyBaseTask):
             self.select_cube_and_equ(cue_box, equ_box)
 
             while True:
-                self.key_down_space_and_find_success_icon()
-                self.sleep(1.5)
+                success_icon = self.key_down_space_and_find_success_icon()
+                if not success_icon:
+                    self.log_error("没有识别到Success特征，切换tab尝试")
+                    self.sleep(2)
+                    self.click(84, 430)
+                    self.sleep(0.4)
+                    self.click(84, 430)
+                    self.sleep(0.4)
+                    self.click(*apd_box.center())
+                    self.sleep(0.4)
+                    self.click(*apd_box.center())
+                    self.sleep(0.4)
+                    self.click(*cue_box.center())
+                    continue
+                self.sleep(1.5 * self.rate)
 
                 res, cue_box = self.check_cube_result(cube=cube)
 
@@ -195,19 +223,21 @@ class WashCubeTask(MyBaseTask):
         self.right_click(equ_box)
         self.sleep(0.1)
 
-    def key_down_space_and_find_success_icon(self):
+    def key_down_space_and_find_success_icon(self) -> Optional[Box]:
         # 按下空格开始洗
         self.send_key('space')
-        self.sleep(0.4)
+        self.sleep(0.4 * self.rate)
         self.send_key('space')
-        self.sleep(0.4)
+        self.sleep(0.4 * self.rate)
         self.send_key('space')
 
-        self.wait_feature(
+        self.log_info('已完成洗魔方空格按下')
+
+        return self.wait_feature(
             feature="Success",
             horizontal_variance=0.1,
             vertical_variance=0.1,
-            threshold=0.8,
+            threshold=0.7,
             time_out=5
         )
 
@@ -230,6 +260,15 @@ class WashCubeTask(MyBaseTask):
                       f'第一条：{ocr_res1}\n'
                       f'第二条：{ocr_res2}\n'
                       f'第三条：{ocr_res3}')
+
+        if '测试' in "".join(self.config['期望属性']):
+            cube_box = self.find_one(
+                feature_name=cube,
+                vertical_variance=0.1,
+                horizontal_variance=0.13,
+                threshold=0.8  # 选中会出现蓝色背景，降低置信度
+            )
+            return True, cube_box
 
         # 将获取结果组装成列表传入
         lines = [ocr_res1.encode('gbk'), ocr_res2.encode('gbk'), ocr_res3.encode('gbk')]
@@ -258,36 +297,31 @@ class WashCubeTask(MyBaseTask):
         return rgb_p > 0.8
 
     def get_result_from_washcube_dll(self, lines: list[bytes], expect_attr: list[str], expect_type: list[str]) -> bool:
-        res_ptr: str = ctypes.cast(self.check_func(*lines), ctypes.c_char_p).value.decode('gbk')
+        res_ptr: str = ctypes.cast(self.check_func(*lines), ctypes.c_char_p).value.decode('utf-8')
+
+        self.info_set("dll返回结果为", res_ptr)
 
         if '垃圾' in res_ptr:
-            self.info_set(key="dll校验结果", value="垃圾")
-            self.log_info("dll校验结果为垃圾")
             return False
 
         res_l = res_ptr.split('|')
         res_type = res_l[0]
         res_attr = res_l[1]
         attr_num = res_l[2]
+        extra_attr = res_l[3]
 
         if res_type not in expect_type:
-            self.info_set(key="期望结果类型不一致", value=f"结果类型为: {res_type}")
-            self.log_info(f"期望结果类型不一致，结果类型为：{res_type}")
             return False
 
         if res_attr not in "".join(expect_attr):
-            self.info_set(key=f"洗的出属性：{res_attr}", value="不在期望属性中")
-            self.log_info(f"洗的出属性：{res_attr}，不在期望属性中")
             return False
 
         # 如果是暴伤或者冷却，那么判断是否符合词条数目
-        if res_attr in ['暴伤', '冷却'] and (attr_num + res_attr) not in "".join(expect_attr):
-            self.info_set(key=f"洗的出属性：{attr_num + res_attr}", value="不在期望属性中")
-            self.log_info(f"洗的出属性：{attr_num + res_attr}，不在期望属性中")
+        if res_attr in ['爆伤', '冷却'] \
+                and (attr_num + res_attr) not in "".join(expect_attr) \
+                or (extra_attr != '未知' and extra_attr not in expect_attr):
             return False
         elif int(attr_num) <= 1:
-            self.info_set(key=f"洗的出属性：{attr_num + res_attr}", value="不在期望属性中")
-            self.log_info(f"洗的出属性：{attr_num + res_attr}，不在期望属性中")
             return False
 
         return True
